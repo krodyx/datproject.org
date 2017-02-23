0e8611a4c619ac68c679f9a9a61060a6b07a4108
karissa <krmckelv@gmail.com>
2017-02-22 18:38:14 -0800

    add login/register buttons to top right

--- client/js/models/user.js
+++ client/js/models/user.js
@@ -22,6 +22,9 @@ module.exports = {
     sidePanel: (state, data) => {
       return {sidePanel: state.sidePanel === 'hidden' ? '' : 'hidden'}
     },
+    registerPanel: (state, showPanel) => {
+      return {register: showPanel ? '' : 'hidden'}
+    },
     loginPanel: (state, showPanel) => {
       return {login: showPanel ? '' : 'hidden'}
     }
